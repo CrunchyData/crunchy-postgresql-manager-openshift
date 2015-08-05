2ab1bee3ac9754524c22af109753b99b7205d0f5
jeffmc <jeffmc@origin.crunchy.lab>
2015-08-05 10:12:54 -0400

    new openshift create pod script

---
+++ sbin/openshift-create-pod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+#
+# $1 - openshift URL
+# $2 - username
+# $3 - password
+# $4 - pod template file
+#
+
+OPENSHIFT_URL=$1
+OPENSHIFT_USERNAME=$2
+OPENSHIFT_PASSWORD=$3
+POD_TEMPLATE_FILE=$4
+
+/var/cpm/bin/oc login $OPENSHIFT_URL --insecure-skip-tls-verify=true \
+	--username=$2 --password=$3
+/var/cpm/bin/oc create -f $POD_TEMPLATE_FILE
